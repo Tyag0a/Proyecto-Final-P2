d5ac8c9e377d8df6e62dc96666f0238276b0c0d6
Tyag0a <santiaguirreo26@gmail.com>
2024-10-26 12:04:59 -0500

    commit de prueba (ignorar)

--- co.edu.uniquindio.proyectofinalp2/src/main/java/co/edu/uniquindio/marketplace/controller/EstadisticasController.java
+++ co.edu.uniquindio.proyectofinalp2/src/main/java/co/edu/uniquindio/marketplace/controller/EstadisticasController.java
@@ -1,4 +1,7 @@
 package co.edu.uniquindio.marketplace.controller;
 
+import co.edu.uniquindio.marketplace.factory.ModelFactory;
+
 public class EstadisticasController {
+    static ModelFactory modelFactory;
 }

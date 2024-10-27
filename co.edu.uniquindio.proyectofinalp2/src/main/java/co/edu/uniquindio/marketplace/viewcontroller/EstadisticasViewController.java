bf68f90e1f3e16d57b52aa8169760fec3b09d14b
Tyag0a <santiaguirreo26@gmail.com>
2024-10-26 20:13:07 -0500

    se agrego logica de views

--- co.edu.uniquindio.proyectofinalp2/src/main/java/co/edu/uniquindio/marketplace/viewcontroller/EstadisticasViewController.java
+++ co.edu.uniquindio.proyectofinalp2/src/main/java/co/edu/uniquindio/marketplace/viewcontroller/EstadisticasViewController.java
@@ -1,5 +1,45 @@
 package co.edu.uniquindio.marketplace.viewcontroller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import co.edu.uniquindio.marketplace.controller.EstadisticasController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+
 public class EstadisticasViewController {
-    //ventana por terminar
+    static EstadisticasController estadisticasController;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnExportarEstadisticas;
+
+    @FXML
+    private DatePicker dtFecha;
+
+    @FXML
+    void onExportarStats(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMostrarFecha(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnExportarEstadisticas != null : "fx:id=\"btnExportarEstadisticas\" was not injected: check your FXML file 'estadisticas-view.fxml'.";
+        assert dtFecha != null : "fx:id=\"dtFecha\" was not injected: check your FXML file 'estadisticas-view.fxml'.";
+
+    }
+
 }
+
--- co.edu.uniquindio.proyectofinalp2/src/main/java/co/edu/uniquindio/marketplace/viewcontroller/MuroViewController.java
+++ co.edu.uniquindio.proyectofinalp2/src/main/java/co/edu/uniquindio/marketplace/viewcontroller/MuroViewController.java
@@ -1,7 +1,43 @@
 package co.edu.uniquindio.marketplace.viewcontroller;
 
+import co.edu.uniquindio.marketplace.controller.MuroController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MuroViewController {
-//por terminar
-// aun no se termina la ventana
+    static MuroController muroController;
+
+    @FXML
+    private Button btnAgregarContact;
+
+    @FXML
+    private Button btnBuscarUser;
+
+    @FXML
+    private Button btnCerrarSesion;
+
+    @FXML
+    private Button btnOpenWindow3;
+
+    @FXML
+    void onAgregarAliado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBuscarUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCerrarSesion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onOpenWindow3(ActionEvent event) {
+
+    }
 
 }
